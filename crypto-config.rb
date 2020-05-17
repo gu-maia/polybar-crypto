b7139355b92d5ae24d2b04a4531a6df493d20352
Gustavo <gu.mmeneses@gmail.com>
2020-05-17 16:38:55 -0300

    removed sample key

--- crypto-config.rb
+++ crypto-config.rb
@@ -1,6 +1,6 @@
 @coins = { :bitcoin => "bitcoin_icon", :dash => "dash_icon" }
 @currency = 'USD'
-@api_key = '80951962-2e65-43e6-a9bc-ff3b9cc11196'
+@api_key = ''
 
 @request_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=#{@coins.keys.join(",")}"
 
