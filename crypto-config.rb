877eb7bd9915a82a84402ad05d5e1d96a6d9a15d
Gustavo <gu.mmeneses@gmail.com>
2020-05-17 16:08:43 -0300

    refatorado com o pedro

--- crypto-config.rb
+++ crypto-config.rb
@@ -1,8 +1,8 @@
-@coins = [[coin: "bitcoin", icon: ""], [coin: "dash", icon: ""]]
+@coins = { :bitcoin => "bitcoin_icon", :dash => "dash_icon" }
 @currency = 'USD'
 @api_key = ''
 
-@request_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=#{@coins.map { |a| [a.first.first[1]] }.join(",")}"
+@request_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=#{@coins.keys.join(",")}"
 
 @headers = {
    "Accepts"  => "application/json",
--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require 'httparty'
+require 'json'
 require_relative 'crypto-config'
 
-response = HTTParty.get(@request_url, :headers => @headers).parsed_response
+response = HTTParty.get(@request_url, :headers => @headers)
 
-counter = 0
-response["data"].each do |current_coin| 
-  print "#{@coins[counter][0][:icon]} #{current_coin[1]["quote"]["USD"]["price"].round(2)} "
-counter += 1
+response["data"].each do |current_coin|
+  icon = "#{@coins[current_coin[1]["slug"].to_sym]}"
+  value = current_coin[1].dig('quote', @currency, 'price')&.round(2)
+  print "#{icon} #{value} "
 end
